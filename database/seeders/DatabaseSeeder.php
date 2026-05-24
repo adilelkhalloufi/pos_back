d389fc09dd909f268a2df73e5fa754ea0472d30a
adil elkhalloufi <adilelkhalloufi@gmail.com>
2026-05-24 14:13:20 +0100

    fix : seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -63,9 +63,8 @@ class DatabaseSeeder extends Seeder
         $this->call(ModePayemntSeeder::class);
         // $this->call(SupplierSeeder::class);
         $this->call(CategorySeeds::class);
-        // $this->call(QuickStartProductSeeder::class);
-        //  $this->call(QuickTestSeeder::class);
-
-       
+        $this->call(QuickStartProductSeeder::class);
+        $this->call(QuickTestSeeder::class);
+        // php artisan products:import-excel
     }
 }
